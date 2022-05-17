56796090a218da751b0201e8c6dee1282cd59cba
oDqnger <103481200+Diman012@users.noreply.github.com>
2022-05-17 18:00:30 +0100

    Added initial changes to aa12pro website

---
+++ index.js
@@ -0,0 +1,26 @@
+
+document.querySelector(".join-server button").addEventListener("click", myFunction);
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("myInput");
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  navigator.clipboard.writeText(copyText.value);
+
+  /* Alert the copied text */
+
+  animateWord();
+}
+
+function animateWord() {
+  document.querySelector(".join-server button").innerHTML = "IP Address Copied";
+
+  setTimeout(function () {
+    document.querySelector(".join-server button").innerHTML = "Copy the IP Address";
+  }, 1000);
+}
