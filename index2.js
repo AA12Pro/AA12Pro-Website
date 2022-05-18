d95f8dc3e3e467889f4940712d2dc3d784f23d23
oDqnger <103481200+Diman012@users.noreply.github.com>
2022-05-18 20:30:39 +0100

    Updating Website

---
+++ index2.js
@@ -0,0 +1,40 @@
+$(".nope1").on("click", function () {
+  $(".nothing1").addClass("r");
+})
+
+$(".nopeL1").on("click", function () {
+  $("nothing1").removeClass("r");
+})
+
+$(".nope2").on("click", function () {
+  $(".nothing2").addClass("r");
+})
+
+$(".nopeL2").on("click", function () {
+  $("nothing2").removeClass("r");
+})
+
+
+$(".nope3").on("click", function () {
+  $(".nothing3").addClass("r");
+})
+
+$(".nopeL3").on("click", function () {
+  $("nothing3").removeClass("r");
+})
+
+$(".nope4").on("click", function () {
+  $(".nothing4").addClass("r");
+})
+
+$(".nopeL4").on("click", function () {
+  $("nothing4").removeClass("r");
+})
+
+$(".nope5").on("click", function () {
+  $(".nothing5").addClass("r");
+})
+
+$(".nopeL5").on("click", function () {
+  $("nothing5").removeClass("r");
+})
